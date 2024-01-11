4e8abd04809cef959aa3678fb08801aee9bb7f2a
Zohair-coder <zohair.ul.hasan@gmail.com>
2024-01-11 14:19:21 -0500

    made MOTION_COMPUTER_IP an environment variable

--- globals.py
+++ globals.py
@@ -1,4 +1,10 @@
-MOTION_COMPUTER_IP = '127.0.0.1'
+import os
+
+# checks the environment variable MOTION_COMPUTER_IP
+# and defaults to localhost if it is not set
+# this will allow us to copy over the code to the motion computer
+# and run it without having to change the code
+MOTION_COMPUTER_IP = os.getenv('MOTION_COMPUTER_IP', '127.0.0.1') 
 MOTION_COMPUTER_PORT = 4001
 NL2_IP = '127.0.0.1'
 NL2_PORT = 15151
