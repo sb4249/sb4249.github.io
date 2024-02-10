f0243752020acd8eafd9549f851a6ccadc07106f
hdc25 <hdc25@drexel.edu>
2024-02-10 18:57:50 -0500

    Created safety mode to hopefully prevent interlock issues. Will need to be tested and tweaked in MSL.

--- math_utils.py
+++ math_utils.py
@@ -1,14 +1,29 @@
 from scipy.spatial.transform import Rotation
 
+lastPitch = 0
+safetyCounter = 0
+safetyMode = False
+
 def quaternion_to_pitch_and_roll(x, y, z, w):
     quaternion = Rotation.from_quat([x, y, z, w])
     eulerAngles = quaternion.as_euler('xzy', degrees=True)
 
     pitch, roll, _ = eulerAngles
+    
+    if (pitch > 0 and pitch-lastPitch >= 0.5): #Will have to be changed with testing. "0.5" should be replaced with the large single-frame delta seen in the test where an interlock occurred.
+        safetyCounter += 1
+    elif (pitch <= 0):
+        safetyMode = False
+        safetyCounter = 0
+    
+    if (safetyCounter >= 5): #Will also have to be changed in testing. Can modify the sensitivity of safety mode.
+        safetyMode = True
+        
+    if (safetyMode):
+        if (pitch > 29.4): #Final thing that may need tweaking. 29.4 is the maximum angle achievable by the hardware, but orchestrated motion may make this more complicated.
+            pitch = pitch * 0.9
 
-    # pitch = max(-15.5, min(pitch, 33.75))
-    # roll = max(-40, min(roll, 40))
-
+    lastPitch = pitch
     return pitch, roll
 
 def calculate_velocity(lastPos, currentPos, timeDiff=0.03333333333):
