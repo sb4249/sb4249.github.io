0f89b91be1cfbcbc863e889589def250e538a83c
hdc25 <hdc25@drexel.edu>
2024-02-18 16:47:17 -0500

    Preliminary pdoc documentation for client.py.

--- client.py
+++ client.py
@@ -1,12 +1,20 @@
+"""This module facilitates communication between the show computer and the motion computer."""
+
 import socket
 
 class Client:
+    """This class contains all of the neccesary functions to initiate communication with the motion computer, send data to the motion computer, and close the socket conenction to the motion computer."""
     def __init__(self, ip, port):
+        """Initializes objects needed for network connection."""
         self.ip = ip
+        """The IP of the motion computer."""
         self.port = port
+        """The port that the motion computer will communicate on."""
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        """The socket object used for communication between the show computer and the motion computer."""
 
     def send_data(self, data):
+        """This function sends a formatted packet of motion data for one frame to the motion computer."""
         try:
             self.client_socket.sendto(data, (self.ip, self.port))
 
