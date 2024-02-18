b59dbb8e8ca8c92fdeeff3e1285bb0a82744528b
hdc25 <hdc25@drexel.edu>
2024-02-18 17:00:41 -0500

    Preliminary documentation for logger.py with pdoc.

--- logger.py
+++ logger.py
@@ -1,3 +1,5 @@
+"""This module handles logging for the application. Will log information recieved from NoLimits2 and the packet that is constructed from it."""
+
 import os
 import datetime
 from nl2telemetry.message.reply import TelemetryData
@@ -5,6 +7,7 @@ from packet import Packet
 
 
 def write(data: str, fileName: str):
+    """This function is the on actually writing data to a log file. It will find the file and write the provided data to it if it is emtpy and append the data if it already contains some."""
 
     #get file path
     fileName += str(datetime.datetime.now())
@@ -16,6 +19,7 @@ def write(data: str, fileName: str):
 
 
 def log_nl2(nl2_data: TelemetryData):
+    """This function retrieves data from NoLimits2, formats it into the correct form for a log file, and then writes it to a log file called `nl2_data`"""
     data = ""
     data += "position x: " + str(nl2_data.position_x) + "\n"
     data += "position y: " + str(nl2_data.position_y) + "\n"
@@ -29,6 +33,7 @@ def log_nl2(nl2_data: TelemetryData):
     write(data=data, fileName="nl2_data")
 
 def log_packet(packetData: Packet):
+    """This function retrieves data from the packets the applcation constructs, formats it into the correct form for a log file, and then writes it to a log file called `packet_data`"""
     data = ""
     data += "x_lin_vel: " + str(packetData.x_lin_vel) + "\n"
     data += "y_lin_vel: " + str(packetData.y_lin_vel) + "\n"
