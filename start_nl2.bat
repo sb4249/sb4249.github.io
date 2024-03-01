cc0188d283cd29a37ebabce7c0908e259fe721ba
hdc25 <hdc25@drexel.edu>
2024-02-29 19:18:20 -0500

    Modified batch script to allow proper startup.

--- start_nl2.bat
+++ start_nl2.bat
@@ -1,5 +1,8 @@
-echo Starting Python script...
-python "C:\path\to\your\script.py"
 echo Starting No Limits 2...
 start "" "C:\path\to\NoLimits2\NoLimits2.exe"
+
+timeout /t 10
+
+echo Starting Python script...
+python "C:\path\to\your\script.py"
 echo Done.
